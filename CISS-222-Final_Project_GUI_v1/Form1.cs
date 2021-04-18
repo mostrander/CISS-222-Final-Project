54876e33efde5ba24f8e6712f43cac30e3652663
Megan Ostrander <Megan@DESKTOP-G32KF0T>
2021-04-17 23:09:59 -0400

    Begun work on the version of final project that uses a GUI.

---
+++ CISS-222-Final_Project_GUI_v1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CISS_222_Final_Project_GUI_v1
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      //Log into an account button
+      private void button1_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      //Create an account button
+      private void button2_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      //Exit program button
+      //Works
+      private void button3_Click(object sender, EventArgs e)
+      {
+         Environment.Exit(0);
+      }
+   }
+}
